fded036be9af22b7dadaefea6fc2cc0b62c38587
MaysaCusma <146853333+MaysaCusma@users.noreply.github.com>
2024-11-05 08:48:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Literatura',
+    'Eram cinco horas da manhã e o cortiço acordava, abrindo, não os olhos, mas a sua infinidade de portas e janelas alinhadas." Esse trecho pertence a qual livro da literatura brasileira?',
+    'O Cortiço (Aluísio Azevedo)'
+)
+
+criaCartao(
+    'Literatura',
+    '"No mais fundo do seu coração ele tinha pena da desgraça de todos." de qual livro da literatura brasileira é este trecho?',
+    'Capitães de areia (Jorge Amado)'
+)
+
+criaCartao(
+    'Literatura',
+    'O seguinte trecho pertence a qual obra? “Quem governa o nosso país é quem tem dinheiro, quem não sabe o que é a dor e a aflição do pobre.”',
+    'Quarto de Despejo, 1960. (Carolina Maria de Jesus)'
+)
+
+criaCartao(
+    'Literatura',
+    '“A liberdade é defendida com discursos e atacada com metralhadoras.” este trecho pertence à qual obra brasileira?',
+    'O Avesso das Coisas, 1990 (Carlos Drummond de Andrade)'
+)
+
+criaCartao(
+    'Literatura',
+    '“Mas a saudade é isto mesmo; é o passar e repassar das memórias antigas” este trecho pertence à qual obra brasileira?',
+    'Dom Casmurro (Machado de Assis)'
+)
+
+criaCartao(
+    'Literatura',
+    'O seguinte trecho pertence a qual obra? “Marcela amou-me durante quinze meses e onze contos de réis.”',
+    'Memórias Póstumas de Brás Cubas (Machado de Assis)'
+)
